7d43640c6c7934e3c95c4431925d14c2e108a809
XLor <yjl9903@vip.qq.com>
2023-04-03 18:37:30 +0800

    fix: cast this var

--- src/main/gen/pascal/taie/intellij/tir/syntax/psi/TirIdentifierVariable.java
+++
@@ -1,8 +0,0 @@
-// This is a generated file. Not intended for manual editing.
-package pascal.taie.intellij.tir.syntax.psi;
-
-import pascal.taie.intellij.tir.syntax.TirNamedElement;
-
-public interface TirIdentifierVariable extends TirNamedElement {
-
-}
