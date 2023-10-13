269442e653d2c051dacf4762441fe43a3b7548d4
Teng Zhang <zhangt2333@gmail.com>
2023-10-13 18:40:41 +0800

    fix: the path of logo file

--- src/main/kotlin/pascal/taie/intellij/tir/TirIcons.kt
+++ src/main/kotlin/pascal/taie/intellij/tir/TirIcons.kt
@@ -4,6 +4,6 @@ import com.intellij.openapi.util.IconLoader
 
 class TirIcons {
     companion object {
-        val PASCAL = IconLoader.getIcon("/icons/pluginIcon.png", TirIcons::class.java);
+        val PASCAL = IconLoader.getIcon("/META-INF/pluginIcon.svg", TirIcons::class.java);
     }
 }
