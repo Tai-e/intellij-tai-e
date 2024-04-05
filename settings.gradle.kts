81687c73c4d719b3add0ac9d1b5308777e1d3c27
Teng Zhang <zhangt2333@gmail.com>
2024-04-05 19:35:33 +0800

    chore: sync with latest JetBrains/intellij-platform-plugin-template

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.name = "intellij-tai-e"
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
